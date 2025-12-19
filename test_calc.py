14b1fb4976af115c784cc9d14a057a3492c94b4b
ronb8 <ronbuaron8@gmail.com>
2025-12-19 12:47:16 +0200

    add comments to asserts in test_calc

--- test_calc.py
+++ test_calc.py
@@ -2,10 +2,10 @@ import pytest
 from calc import add, divide
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 5, "Expected add(2, 3) to be 5"
 
 def test_divide():
-    assert divide(10, 2) == 5
+    assert divide(10, 2) == 5, "Expected divide(10, 2) to be 5"
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):

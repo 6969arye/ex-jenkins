48e1ce2d7f854e78d58ee6e83eb278fa80e7f581
ronb8 <ronbuaron8@gmail.com>
2025-12-19 13:00:56 +0200

    add test_substract to test_calc.py file

--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,5 @@
 import pytest
-from calc import add, divide
+from calc import add, divide, substract
 
 def test_add():
     assert add(2, 3) == 5, "Expected add(2, 3) to be 5"
@@ -10,3 +10,6 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         divide(10, 0)
+
+def test_subtract():
+    assert substract(10, 2) == 7, "Expected subtract(10, 2) to be 8"
